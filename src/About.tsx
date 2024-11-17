6263e76bd22df57117855bfb162d044fb382e8f9
virysik <68701496+virysik@users.noreply.github.com>
2024-11-17 19:36:06 +0100

    update about section, tailwind classes

--- src/About.tsx
+++ src/About.tsx
@@ -1,19 +1,67 @@
 function About() {
   return (
-    <section>
-      <h2>About Us</h2>
-      <div>
-        <div>
-          <h3>Who We Are</h3>
-          <p>
+    <section className="section">
+      <h2 className="mb-1 flex justify-center font-semibold tracking-wide sm:text-[1.125rem] lg:text-[1.625rem] 2xl:text-[3rem]">
+        About Us
+      </h2>
+      <div className="md:flex md:flex-row gap-16 justify-between">
+        <div className="max-md:mb-4 md:pt-10 md:pb-10 lg:pt-10 lg:pb-24 xl:pt-20 xl:pb-60 md:w-4/12">
+          <h3 className="pb-1 sm:pb-2 lg:pb-5 font-semibold tracking-wide sm:text-[1.125rem] lg:text-[1.625rem] 2xl:text-[3rem]">
+            Who We Are
+          </h3>
+          <p className="text-[1rem] md:text-[1.1rem] lg:text-[1.15rem] xl:text-[2rem] leading-loose">
             We pride ourselves on being the most loved furniture store in the
             world. We've got everything from couches, to beds, to dining tables,
             and much more.
           </p>
         </div>
-        <img src="" alt="about1" />
-        <img src="" alt="about2" />
-        <img src="" alt="about3" />
+        <div className="md:w-8/12 md:flex gap-6 justify-between">
+          <picture className="block max-w-full w-72 mt-auto max-md:mb-4">
+            <source
+              srcSet="src/assets/about1@1x.webp 1x, src/assets/about1@2x.webp 2x"
+              type="image/webp"
+            />
+            <source
+              srcSet="src/assets/about1@1x.jpg 1x, src/assets/about1@2x.jpg 2x"
+              type="image/jpeg"
+            />
+            <img
+              src="src/assets/about1@2x.jpg"
+              alt="about1"
+              className="block w-full"
+            />
+          </picture>
+          <picture className="block max-w-full w-72 mt-auto max-md:mb-4 max-md:ml-auto">
+            <source
+              srcSet="src/assets/about2@1x.webp 1x, src/assets/about2@2x.webp 2x"
+              type="image/webp"
+            />
+            <source
+              srcSet="src/assets/about2@1x.jpg 1x, src/assets/about2@2x.jpg 2x"
+              type="image/jpeg"
+            />
+            <img
+              src="src/assets/about2@2x.jpg"
+              alt="about2"
+              className="block w-full"
+            />
+          </picture>
+          <picture className="block max-w-full w-72 md:mb-auto">
+            <source
+              srcSet="src/assets/about3@1x.webp 1x, src/assets/about3@2x.webp 2x"
+              type="image/webp"
+            />
+            <source
+              srcSet="src/assets/about3@1x.jpg 1x, src/assets/about3@2x.jpg 2x"
+              type="image/jpeg"
+            />
+            <img
+              src="src/assets/about3@2x.jpg"
+              alt="about3"
+              className="block w-full "
+            />
+          </picture>
+        </div>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,19 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
       },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
